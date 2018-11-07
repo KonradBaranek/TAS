473430a707e596dbc6522ab75cfe2b43ca6d062c
KonradBaranek <konrad.baranek01@gmail.com>
2018-11-07 17:08:22 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,3 @@
 //Dajcie na ten plik git ignore zebysmy nie nadpisywali swoich baz
-const dataBase = "mongodb://tas:tas123@ds131753.mlab.com:31753/tasy"
+const dataBase = ""
 module.exports = dataBase
