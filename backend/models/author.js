115acc426a9f7b54c0632904441d9c72d501f95b
Krzysztof Grefling <grefling.krzysztof@gmail.com>
2019-01-28 06:50:59 +0100

    authors backend

--- backend/models/author.js
+++ backend/models/author.js
@@ -4,13 +4,24 @@ var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
   {
-    name: {type: String, required: true, max: 100},
-    surname: {type: String, required: true, max: 100},
-    dateOfBirth: {type: Date},
-    dateOfDeath: {type: Date},
+    IDAuthor: {type: Number, required: true}, 
+    name: {type: String, required: true, max: 50},
+    surname: {type: String, required: true, max: 50},
+    genre: {type: String,  enum: ["fantasy","mystery","romance","thriller","drama","adventure"],  required: false},
+    dateOfBirth: {type: Date, required: true},
+    dateOfDeath: {type: Date, required: false},
     descritpion: {type: String, required: false}
   }
 )
 
+AuthorSchema.methods.add = function (){
+  this.findOne({IDAuthor: this.IDAuthor}).then( author =>{
+    if(author){
+      console.log(`author with isbn: ${this.IDAuthor} already exists`)
+    }else{
+      this.create(this)
+    }
+  })
+}
 
 module.exports = mongoose.model('Author', AuthorSchema);
